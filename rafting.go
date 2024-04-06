5c2ddec11b65b942d0e2917c0df2bbe12c92e1d4
edwinjavier28 <erico.andes@gmail.com>
2024-04-05 19:15:57 -0500

    new file rafting.go

---
+++ rafting.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Definimos dos números para multiplicar
+	a := 5
+	b := 3
+
+	// Realizamos la multiplicación
+	resultado := a * b
+
+	// Mostramos el resultado
+	fmt.Println("El resultado de la multiplicación es:", resultado)
+}
